95b02e599b78087ebeac9f2a73fccad3c558d529
lishuai <1946754214@qq.com>
2022-07-06 01:24:02 +0800

    data precess audio2csv

--- data/dataprecess.py
+++ data/dataprecess.py
@@ -8,20 +8,21 @@ from utils.logger import LOGGER
 from config.config import shared_configs
 
 
-def read_wav_csv(audio_path):
+def read_wav_csv(audio_path,audio_csv_path):
     if not audio_path.exists():
         LOGGER.error("read_wav_csv audio_path:%s not exist.",audio_path)
         return
     file_list = os.listdir(audio_path)
     df = pd.DataFrame()
     df.columns = ["id","audio_name","audio_data"]
-    for file in file_list:
+    for id, file in enumerate(file_list):
         wave_data, samplerate = librosa.load(file)
-
+        df.loc[id] = [id,file[:-4],wave_data]
+    df.to_csv(audio_csv_path)
 
 
 def cmumosei_data_process(args):
-    audio_path = args.cmumosei__audio_path
+    audio_path = args.cmumosei_audio_path
     video_path = args.cmumosei_video_path
 
 
