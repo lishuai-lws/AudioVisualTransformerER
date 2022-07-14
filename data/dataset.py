2ff270adb8d8395c7341076b7d99a097bc3e3cc0
lishuai <1946754214@qq.com>
2022-07-15 00:26:42 +0800

    0714

--- data/dataset.py
+++ data/dataset.py
@@ -4,19 +4,36 @@ from config.config import shared_configs
 import  os
 import librosa
 import torchvision.transforms as transformer
-
+from PIL import Image
+import numpy as np
 
 class CMUMOSEIDataset(Dataset):
     def __init__(self, audio_path, video_path, ids_path):
         ids = pd.read_csv(ids_path).tolist()
         self.videoTransformer = transformer.Compose([
-            transformer.
+            transformer.CenterCrop(),
             transformer.ToTensor()
         ])
-        waveDatas = []
+        wavedatas = []
+        videodatas = []
         for id in ids:
             wave_data, samplerate = librosa.load(os.path.join(audio_path, id+".wav"))
-            waveDatas.append(wave_data)
+            wavedatas.append(wave_data)
+            videodir = os.path.join(video_path,id+"_aligend")
+            imglist = os.listdir(videodir)
+            video = []
+            for image in imglist:
+                imgpath = os.path.join(videodir,image)
+                img = Image.open(imgpath)
+                video.append(np.array(img.getdata()))
+                img.close()
+            videodatas.append(video)
+        self.audiodata = wavedatas
+        self.videodata = videodatas
+
 
     def __len__(self):
         return len(self.ids)
+
+    def __getitem__(self, item):
+        return self.audiodata[item], self.videodata[item]
