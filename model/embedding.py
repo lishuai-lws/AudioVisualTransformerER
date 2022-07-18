feb2cfab9c315f953594115a03d03fdcb099532b
lishuai <1946754214@qq.com>
2022-07-19 00:14:11 +0800

    0714

--- model/embedding.py
+++ model/embedding.py
@@ -1,8 +1,15 @@
 from torch import nn
-
+from transformers import Wav2Vec2Tokenizer, Wav2Vec2Model
 
 
 
 class AudioWav2Vec2(nn.Module):
-    def __init__(self):
+    def __init__(self,modelpath):
         super().__init__()
+        self.tokenizer = Wav2Vec2Tokenizer.from_pretrained(modelpath,padding=True)
+        self.model = Wav2Vec2Model.from_pretrained(modelpath)
+
+    def forward(self, wavdata):
+        data = self.tokenizer(wavdata)
+        feature = self.model(data)
+        return feature
