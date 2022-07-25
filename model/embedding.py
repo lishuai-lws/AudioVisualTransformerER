36ac00e8b560c4e04e8c87d5c4d59eba90c41824
lishuai <1946754214@qq.com>
2022-07-26 01:09:22 +0800

    0726

--- model/embedding.py
+++ model/embedding.py
@@ -1,5 +1,7 @@
 from torch import nn
 from transformers import Wav2Vec2Tokenizer, Wav2Vec2Model
+from transformers import AutoFeatureExtractor, ResNetForImageClassification
+
 
 
 
@@ -10,6 +12,18 @@ class AudioWav2Vec2(nn.Module):
         self.model = Wav2Vec2Model.from_pretrained(modelpath)
 
     def forward(self, wavdata):
-        data = self.tokenizer(wavdata)
+        data = self.tokenizer(wavdata, return_tensors="pt").input_values
         feature = self.model(data)
+        return feature
+
+
+class ResNet50(nn.Module):
+    def __init__(self, modelpath):
+        super.__init__()
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(modelpath)
+        self.model = ResNetForImageClassification.from_pretrained(modelpath)
+
+    def forward(self, image):
+        inputs = self.feature_extractor(image, return_tensors="pt")
+        feature = self.model(**inputs)
         return feature
